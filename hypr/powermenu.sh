c5a27c8577ec367bd2ce56284ebb1bde77787d6f
D4-CTM <josuedelcid325@gmail.com>
2025-11-03 22:05:01 -0600

    Hyprland condig modified

--- hypr/powermenu.sh
+++ hypr/powermenu.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-chosen=$(printf "  Power Off\n  Reboot\n  Suspend\n" | wofi --dmenu --prompt "Power Menu")
+chosen=$(printf "Power Off\nReboot\nSuspend\n" | wofi --dmenu --prompt "Power Menu")
 
 case "$chosen" in
-    "  Power Off") systemctl poweroff ;;
-    "  Reboot") systemctl reboot ;;
-    "  Suspend") systemctl suspend ;;
-    "  Lock") hyprlock ;;
-    "  Logout") hyprctl dispatch exit ;;
+    "Power Off") systemctl poweroff ;;
+    "Reboot") systemctl reboot ;;
+    "Suspend") systemctl suspend ;;
 esac
 
