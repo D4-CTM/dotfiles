4bed94445c3f388319bcdc1bd1da25f41aef8435
D4-CTM <josuedelcid325@gmail.com>
2026-03-08 11:24:04 -0600

    Copilot plugin reimplemented for test. Changed rust_analyzer config. Changed zen-browser to brave. Tmux.conf fixed

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,45 +31,45 @@ setKey("n", "r", ":redo<CR>")
 setKey({ "n", "v" }, "Y", [["+y]])
 
 vim.keymap.set("n", "<leader>lg", function()
-	local fzf = require("fzf-lua")
+    local fzf = require("fzf-lua")
 
-	vim.ui.input({ prompt = "Input file pattern: " }, function(input)
-		if input == nil or input == "" then
-			vim.notify("File extension was empty", vim.log.levels.ERROR)
-			return
-		end
+    vim.ui.input({ prompt = "Input file pattern: " }, function(input)
+        if input == nil or input == "" then
+            vim.notify("File extension was empty", vim.log.levels.ERROR)
+            return
+        end
 
-		fzf.live_grep({
-			rg_opts = string.format("--glob %s", input)
-		})
-	end)
+        fzf.live_grep({
+            rg_opts = string.format("--glob %s", input)
+        })
+    end)
 end)
 
 vim.api.nvim_create_autocmd("FileType", {
-	callback = function()
-		local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
-		if lang then
-			pcall(vim.treesitter.start)
-		end
-	end,
+    callback = function()
+        local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+        if lang then
+            pcall(vim.treesitter.start)
+        end
+    end,
 })
 
 vim.lsp.enable({
-	"arduino_language_server",
-	"rust_analyzer",
-	"basedpyright",
-	"tinymist",
-	"lemminx",
-	"roslyn",
-	"lua_ls",
-	"clangd",
-	"vue_ls",
-	"cssls",
-	"gopls",
-	"jdtls",
-	"ts_ls",
-	"html",
-	"zls"
+    "arduino_language_server",
+    "rust_analyzer",
+    "basedpyright",
+    "tinymist",
+    "lemminx",
+    "roslyn",
+    "lua_ls",
+    "clangd",
+    "vue_ls",
+    "cssls",
+    "gopls",
+    "jdtls",
+    "ts_ls",
+    "html",
+    "zls"
 })
 
 setKey("n", "F", vim.lsp.buf.format)
@@ -78,39 +78,40 @@ setKey("n", "<leader>gd", vim.lsp.buf.definition)
 setKey("n", "?", vim.lsp.buf.hover)
 
 local function jump(_count, _severity)
-	if _severity == nil then
-		vim.diagnostic.jump({ count = _count, float = true, wrap = true })
-		return
-	end
-	vim.diagnostic.jump({ count = _count, float = true, wrap = true, severity = _severity })
+    if _severity == nil then
+        vim.diagnostic.jump({ count = _count, float = true, wrap = true })
+        return
+    end
+    vim.diagnostic.jump({ count = _count, float = true, wrap = true, severity = _severity })
 end
 
 setKey("n", "<e", function()
-	jump(-1, vim.diagnostic.severity.ERROR)
+    jump(-1, vim.diagnostic.severity.ERROR)
 end)
 setKey("n", ">e", function()
-	jump(1, vim.diagnostic.severity.ERROR)
+    jump(1, vim.diagnostic.severity.ERROR)
 end)
 setKey("n", "<d", function()
-	jump(-1, nil)
+    jump(-1, nil)
 end)
 setKey("n", ">d", function()
-	jump(1, nil)
+    jump(1, nil)
 end)
 
 -- packages
 vim.pack.add({
-	{ src = "https://github.com/seblyng/roslyn.nvim" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-	{ src = "https://github.com/Saghen/blink.cmp",               name = "blink-cmp" },
-	{ src = "https://github.com/OXY2DEV/markview.nvim",          name = "markview" },
-	{ src = "https://github.com/lewis6991/gitsigns.nvim",        name = "gitsigns" },
-	{ src = "https://github.com/nvim-lualine/lualine.nvim",      name = "lualine" },
-	{ src = "https://github.com/ibhagwan/fzf-lua",               name = "fzf-lua" },
-	{ src = "https://github.com/navarasu/onedark.nvim",          name = "onedark" },
-	{ src = "https://github.com/mason-org/mason.nvim",           name = "mason" },
-	{ src = "https://github.com/chomosuke/typst-preview.nvim",   name = "typst" },
-	{ src = "https://github.com/stevearc/oil.nvim",              name = "oil" },
+    { src = "https://github.com/nvim-mini/mini.completion",      name = "mini-cmp" },
+    { src = "https://github.com/nvim-mini/mini.icons",           name = "mini-icons" },
+    { src = "https://github.com/OXY2DEV/markview.nvim",          name = "markview" },
+    { src = "https://github.com/lewis6991/gitsigns.nvim",        name = "gitsigns" },
+    { src = "https://github.com/nvim-lualine/lualine.nvim",      name = "lualine" },
+    { src = "https://github.com/navarasu/onedark.nvim",          name = "onedark" },
+    { src = "https://github.com/github/copilot.vim",             name = "copilot" },
+    { src = "https://github.com/ibhagwan/fzf-lua",               name = "fzf-lua" },
+    { src = "https://github.com/mason-org/mason.nvim",           name = "mason" },
+    { src = "https://github.com/stevearc/oil.nvim",              name = "oil" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+    { src = "https://github.com/seblyng/roslyn.nvim" },
 })
 vim.cmd.packadd("nvim-treesitter")
 
@@ -124,72 +125,53 @@ require("oil").setup()
 require("gitsigns").setup()
 require("markview").setup()
 
-local blink = require("blink.cmp")
-blink.setup({
-	fuzzy = {
-		implementation = "prefer_rust",
-		prebuilt_binaries = {
-			force_version = "1.*"
-		}
-	},
-	keymap = {
-		['<S-tab>'] = {
-			function()
-				if blink.is_visible() then
-					blink.select_and_accept()
-				else
-					vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<S-tab>", true, false, true), "n", true)
-				end
-			end,
-			mode = { 'i' },
-		}
-	},
-})
+require("mini.completion").setup()
+require("mini.icons").setup()
 
 local ts = require("nvim-treesitter")
 ts.install({
-	'dockerfile',
-	'javascript',
-	'typescript',
-	'c_sharp',
-	'arduino',
-	'python',
-	'razor',
-	'typst',
-	'java',
-	'html',
-	'yaml',
-	'rust',
-	'css',
-	'xml',
-	'lua',
-	'zig',
-	'cpp',
-	"vue",
-	'go',
-	'c'
+    'dockerfile',
+    'javascript',
+    'typescript',
+    'c_sharp',
+    'arduino',
+    'python',
+    'razor',
+    'typst',
+    'java',
+    'html',
+    'yaml',
+    'rust',
+    'css',
+    'xml',
+    'lua',
+    'zig',
+    'cpp',
+    "vue",
+    'go',
+    'c'
 }):wait()
 
 ts.setup({
-	install_dir = vim.fn.stdpath('data') .. '/site',
+    install_dir = vim.fn.stdpath('data') .. '/site',
 })
 
 local onedark = require("onedark")
 onedark.setup({
-	style = "deep",
-	transparent = true,
-	code_style = {
-		keywords = "bold",
-		functions = "bold",
-	},
-	highlights = {
-		["@variable"] = { fg = "$red" },
-	},
+    style = "deep",
+    transparent = true,
+    code_style = {
+        keywords = "bold",
+        functions = "bold",
+    },
+    highlights = {
+        ["@variable"] = { fg = "$red" },
+    },
 })
 onedark.load()
 
 require("lualine").setup({
-	options = {
-		theme = "onedark",
-	},
+    options = {
+        theme = "onedark",
+    },
 })
